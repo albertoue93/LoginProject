1b4252550a4cce8f2415c6a71d9edf7364850d17
Alberto Urbina <49090252+albertoue93@users.noreply.github.com>
2021-06-25 18:08:38 -0600

    segunda pantalla admin cambios

--- app/src/main/java/com/example/registration/MyAdapter.java
+++ app/src/main/java/com/example/registration/MyAdapter.java
@@ -14,6 +14,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.text.DateFormat;
@@ -33,6 +35,7 @@ class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     private List<Model> mListOriginal;
     Model meta;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
 
     public MyAdapter(ShowActivity activity, List<Model> mList) {
         this.activity = activity;
@@ -82,6 +85,7 @@ class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         Intent intent = new Intent(activity, MainActivity.class);
         intent.putExtras(bundle);
         activity.startActivity(intent);
+
     }
 
     public void deleteData(int position){
